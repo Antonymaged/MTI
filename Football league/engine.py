3d2f95becf80b5d0a1e878733716ee2adff8604b
Antonymaged <magedantony1@gmail.com>
2024-12-22 01:47:57 +0200

    by the thired commit i have add the ablility for the website to query all the data in the same table name as the page and display it in the terminal

--- Football league/engine.py
+++ Football league/engine.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 SERVER = 'Tony\\SQLEXPRESS'
-DATABASE = 'master'
+DATABASE = 'league'
 DRIVER = 'Microsoft ODBC Driver 17 for SQL Server'
 USERNAME = 'test'
 PASSWORD = 'test'
-DATABASE_CONNECTION = f"mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/master?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
+DATABASE_CONNECTION = f"mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
 engine = create_engine(DATABASE_CONNECTION)
--- Football league/main.py
+++ Football league/main.py
@@ -1,20 +1,58 @@
-from flask import Flask,render_template, request, redirect, session
+from flask import Flask, render_template
+from sqlalchemy import create_engine, text
 from engine import engine
-from sqlalchemy import text
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "SYNTAX CAN DO IT";
-connection = engine.connect()
+app.config['SECRET_KEY'] = "SYNTAX CAN DO IT"
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template("home.html")
-    
-@app.route('/player')
-def players():
-    return render_template("./templates/players.html")
 
-connection.close()
+@app.route('/players')
+def players():
+    with engine.connect() as connection:
+        query = text("SELECT * FROM players")
+        result = connection.execute(query)
+        rows = result.fetchall()
+        for row in rows:
+            print(row)
+
+    return render_template("players.html")
+
+@app.route('/clubs')
+def clubs():
+    with engine.connect() as connection:
+        query = text("SELECT * FROM clubs")
+        result = connection.execute(query)
+        rows = result.fetchall()
+        for row in rows:
+            print(row)
+
+    return render_template("clubs.html")
+
+@app.route('/matches')
+def matches():
+    with engine.connect() as connection:
+        query = text("SELECT * FROM matches")
+        result = connection.execute(query)
+        rows = result.fetchall()
+        for row in rows:
+            print(row)
+
+    return render_template("matches.html")
+
+@app.route('/league')
+def league():
+    with engine.connect() as connection:
+        query = text("SELECT * FROM league")
+        result = connection.execute(query)
+        rows = result.fetchall()
+        for row in rows:
+            print(row)
+
+    return render_template("league.html")
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
